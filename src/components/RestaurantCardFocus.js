178c85547a0e6482a7b7359db3163865b9fd09a6
Leighton <leighton.l14@gmail.com>
2022-04-06 23:51:20 +1000

    17th Commit - Added fetch for all restaurants in rails api

--- src/components/RestaurantCardFocus.js
+++ src/components/RestaurantCardFocus.js
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { getRestaurant } from "../dummydata";
+// import { useParams } from "react-router-dom";
+// import { getRestaurant } from "../dummydata";
 import { Link } from "react-router-dom";
 import { Typography } from "@mui/material";
 import { Rating } from "@mui/material";
@@ -8,17 +8,20 @@ import { Rating } from "@mui/material";
 //top 3 dishes, menu(pop open), top-voted description, more reviews
 //pass props again all the way down from RestaurantCardsList to get all info (use redux to stop prop drilling)
 function RestaurantCardFocus() {
-  let params = useParams();
-  let restaurant = getRestaurant(params.restaurantId);
+  // let params = useParams();
+  // let restaurant = getRestaurant(params.restaurantId);
   return (
     <>
-      <p>Category: {restaurant.category}</p>
-      <h1>{restaurant.name}</h1>
+      <Typography>
+        <h1>Restaurant:</h1>
+      </Typography>
+      {/* <p>Category: {restaurant.category}</p>
+      <h2>{restaurant.name}</h2>
       <p>{restaurant.description}</p>
       <Typography>Rating:</Typography>
       <Rating name="read-only" value={restaurant.rating} readOnly />
       <p>Location: {restaurant.location}</p>
-      <p>Pricing: {restaurant.pricing}</p>
+      <p>Pricing: {restaurant.pricing}</p> */}
       <Link to="/">Home</Link>
     </>
   );
--- src/components/RestaurantCardsList.js
+++ src/components/RestaurantCardsList.js
@@ -2,16 +2,41 @@ import RestaurantCard from "./RestaurantCard";
 import { Grid } from "@mui/material";
 import { getRestaurants } from "../dummydata";
 import { Link, Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 //map over data fetched from Rails API and display the Restaurants as Cards in a Grid
 //pass fetched data as props into RestaurantCard(e.g. name, rating, description, reviews etc.)
 //render those in RestaurantCard and RestaurantCardFocus
 export default function RestaurantCardsList() {
-  let restaurants = getRestaurants();
+  // let restaurants = getRestaurants();
+  //useState
+  const [restaurants, setRestaurants] = useState([]);
+  const [ratings, setRatings] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
+  //useEffect
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/restaurants")
+      .then((response) => response.json())
+      .then((data) => setRestaurants(data));
+  }, []);
+
+  //fetch ratings
+
+  // const getRestaurants = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:3000/api/v1/restaurants");
+  //     const data = await response.json();
+  //     console.log(data);
+  //     return setRestaurants(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <>
       <Link to="/">Home</Link>
-      <div>Random Restaurants</div>
+      <div>Random Restaurants!</div>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {restaurants.map((restaurant, index) => (
           <Grid key={index} item xs={6}>
@@ -27,6 +52,7 @@ export default function RestaurantCardsList() {
                 location={restaurant.location}
                 pricing={restaurant.pricing}
               ></RestaurantCard>
+              <div>Hello {restaurant.name}</div>
             </Link>
           </Grid>
         ))}
